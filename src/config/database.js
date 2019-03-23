7c02ada27767dca29001e2c6f5e76a7be4d59bbd
Rafael Silva <Rafaell_21@hotmail.com>
2019-03-23 20:02:29 -0300

    Erro ao criar usuário

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'mysql08-farm76.kinghost.net',
+  database: 'gladstone03',
+  username: 'gladstone03',
+  password: 'rafael12',
+
+  operatorsAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
